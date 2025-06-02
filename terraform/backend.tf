1483fbea4526dbecdd4da3e72a93710fff24ee50
Modesta55 <uchechimodesta97@gmail.com>
2025-06-02 15:13:39 +0100

    commit33

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.78.0"
+      version = ">= 4.31.0"
     }
     helm = {
       source = "hashicorp/helm"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,25 +2,46 @@ provider "azurerm" {
   features {}
 }
 
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
+}
 
 
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.gp25.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.gp25.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.gp25.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.gp25.kube_config.0.cluster_ca_certificate)
+    host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
   }
 }
 
-resource "helm_release" "external_nginx" {
-  name = "external"
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  chart      = "ingress-nginx"
+  namespace  = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
 
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  namespace        = "ingress"
   create_namespace = true
-  version          = "4.8.0"
 
-  values = [file("${path.module}/values/ingress.yaml")]
+  values = [
+    <<EOF
+controller:
+  replicaCount: 2
+  nodeSelector:
+    "kubernetes.io/os": linux
+  service:
+    type: LoadBalancer
+  admissionWebhooks:
+    enabled: true
+EOF
+  ]
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
 }
+
