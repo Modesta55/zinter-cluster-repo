d9b684018e8fbe53172ed6c5d51318eb5fedb662
Modesta55 <uchechimodesta97@gmail.com>
2025-06-02 15:39:07 +0100

    commit45

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,19 +3,21 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.cluster_ca_certificate)
 }
+
 
 
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
+    host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.cluster_ca_certificate)
   }
 }
+
 
